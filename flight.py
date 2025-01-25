f3ab41bcfe5c1426ac07dc2a79fd64f78d0fadb4
JeremiahB2289 <biggsj3@rpi.edu>
2025-01-24 22:07:58 -0500

    Added api setup

--- flight.py
+++ flight.py
@@ -3,6 +3,14 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes and origins
+API_KEY = "QRwlkKzlY2x5TIXL1VPww"
+BASE_URL = "https://www.carboninterface.com/api/v1"
+
+# Headers for authentication
+headers = {
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
+}
 
 @app.route('/', methods=['POST'])
 def calculate_footprint():
